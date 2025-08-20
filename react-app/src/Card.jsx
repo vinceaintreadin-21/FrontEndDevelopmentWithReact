5ae7f204205b299f2d42c8b24e875624c5e123e2
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-08-20 19:34:10 +0800

    react-app

---
+++ react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card(){
+    return (
+        <div className="card">
+            <h2>Vincent</h2>
+            <p>I code</p>
+        </div>
+    );
+}
+
+export default Card;
